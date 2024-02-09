54ada2cd80fc1e417795ebe60eb3fb633b6d9f1d
Mr. Sahib <140795892+FelixJotta@users.noreply.github.com>
2024-02-09 15:32:30 +0400

    Update NewWebLog.py

--- NewWebLog.py
+++ NewWebLog.py
@@ -1,49 +1,20 @@
-import http.server
-import socketserver
-import cgi
+import streamlit as st
 
-users = {'user1': {'username': 'user1', 'password': 'password1'}}
-
-class SimpleHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            with open('login.html', 'rb') as file:
-                self.wfile.write(file.read())
-        else:
-            super().do_GET()
-
-    def do_POST(self):
-        if self.path == '/login':
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length).decode('utf-8')
-            form_data = cgi.parse_qs(post_data)
-
-            username = form_data.get('username', [''])[0]
-            password = form_data.get('password', [''])[0]
-
-            user = users.get(username)
-
-            if user and user['password'] == password:
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'Login successful! Welcome to the Dashboard.')
-            else:
-                self.send_response(401)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'Login failed. Please check your username and password.')
-        else:
-            super().do_POST()
-
-if __name__ == "__main__":
-    PORT = 8000
-
-    with socketserver.TCPServer(("", PORT), SimpleHandler) as httpd:
-        print(f"Serving on port {PORT}")
-        httpd.serve_forever()
-
-print("hoppan")
+backroundColor = "#40E0D0"
+font = "Bold"
+ 
+st.header("DUMBELEK")
+if st.button("Turana sox"):
+    st.write("https://www.youtube.com/watch?v=jnEWf3LExvI")
+if st.button("Murada sox"):
+    st.write("https://www.youtube.com/shorts/wIWsu9eaU3g")
+if st.button("Imrana sox"):
+    st.write("https://www.youtube.com/shorts/fNfs7qvpE_A")
+if st.button("Raula sox"):
+    st.write("https://www.youtube.com/watch?v=ohdre-Z8sYc")
+if st.button("Nijata sox"):
+    st.write("https://www.youtube.com/watch?v=zpI-tLw4Rmw")
+if st.button("A1000e sox"):
+    st.write("https://www.youtube.com/shorts/-HbQn7JmWjw")
+if st.button("Sahibe sox"):
+    st.write("https://www.instagram.com/p/CzS7-FGNjie/")
